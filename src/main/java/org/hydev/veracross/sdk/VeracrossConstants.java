3d25f1d9a68b9e39f2e5513b4b061b92f2776f6f
Hykilpikonna <admin@moecraft.cc>
2019-08-19 13:57:23 +0800

    [+] Create constants class

---
+++ src/main/java/org/hydev/veracross/sdk/VeracrossConstants.java
@@ -0,0 +1,16 @@
+package org.hydev.veracross.sdk;
+
+/**
+ * This class stores the constants.
+ * <p>
+ * Class created by the HyDEV Team on 2019-08-19!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-08-19 13:56
+ */
+public class VeracrossConstants
+{
+    
+}
