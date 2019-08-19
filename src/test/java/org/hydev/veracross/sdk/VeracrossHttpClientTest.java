a3e7bae7c3ea730f18ace50f74aa5dec42b74bf3
Hykilpikonna <admin@moecraft.cc>
2019-08-19 14:34:06 +0800

    [T] Create tester class

---
+++ src/test/java/org/hydev/veracross/sdk/VeracrossHttpClientTest.java
@@ -0,0 +1,20 @@
+package org.hydev.veracross.sdk;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This class tests {@code VeracrossHttpClient} class.
+ * <p>
+ * Class created by the HyDEV Team on 2019-08-19!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-08-19 14:29
+ */
+class VeracrossHttpClientTest
+{
+}
