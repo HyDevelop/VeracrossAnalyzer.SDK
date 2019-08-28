e7fb2e583d886b2ac60cd3af45eb9a1200131307
Hykilpikonna <admin@moecraft.cc>
2019-08-29 07:04:54 +0800

    [+] Create login method to login with username and password

--- src/main/java/org/hydev/veracross/sdk/StJohnsHttpClient.java
+++ src/main/java/org/hydev/veracross/sdk/StJohnsHttpClient.java
@@ -1,5 +1,17 @@
 package org.hydev.veracross.sdk;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 /**
  * HTTP Client for the veracross sdk specific to SJP.
  * <p>
@@ -12,5 +24,55 @@ package org.hydev.veracross.sdk;
  */
 public class StJohnsHttpClient extends GeneralHttpClient
 {
+    /**
+     * Login and save the session
+     */
+    public void login(String username, String password)
+    {
+        // Combine the login url
+        String loginUrl = "https://www.stjohnsprep.org/userlogin.cfm?do=login&p=114";
+
+        // Create param pairs
+        List<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("username", username));
+        params.add(new BasicNameValuePair("password", password));
+        params.add(new BasicNameValuePair("submit", "login"));
+
+        try
+        {
+            // Create a post request.
+            HttpPost request = new HttpPost(loginUrl);
+
+            // Add headers
+            request.addHeader("accept", "text/html");
+            request.addHeader("accept-language", "en-US");
+            request.addHeader("content-type", "application/x-www-form-urlencoded");
+
+            // Create from entity from param pairs
+            UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(params, "UTF-8");
+            request.setEntity(formEntity);
+
+            // Send request
+            CloseableHttpResponse response = httpClient.execute(request);
+            int status = response.getStatusLine().getStatusCode();
+            String responseText = EntityUtils.toString(response.getEntity(), "UTF-8");
+
+            // Debug output TODO: Remove this
+            System.out.println("Status: " + status);
+            System.out.println("Text: " + responseText);
+
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            // This would never happen during runtime if the JVM encoding
+            // is correctly configured to UTF-8.
+            throw new RuntimeException(e);
+        }
+        catch (IOException e)
+        {
+            // There is not much to do.
+            throw new RuntimeException("Veracross SDK: Login Failed", e);
+        }
+    }
 
 }
