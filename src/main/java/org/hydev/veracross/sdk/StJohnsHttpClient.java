698a3f9d1c10432826c8a89cba2420d63c6e9080
Hykilpikonna <admin@moecraft.cc>
2019-08-29 07:03:06 +0800

    [+] Create a http client for SJP login

---
+++ src/main/java/org/hydev/veracross/sdk/StJohnsHttpClient.java
@@ -0,0 +1,16 @@
+package org.hydev.veracross.sdk;
+
+/**
+ * HTTP Client for the veracross sdk specific to SJP.
+ * <p>
+ * Class created by the HyDEV Team on 2019-08-19!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-08-19 13:45
+ */
+public class StJohnsHttpClient extends GeneralHttpClient
+{
+
+}
