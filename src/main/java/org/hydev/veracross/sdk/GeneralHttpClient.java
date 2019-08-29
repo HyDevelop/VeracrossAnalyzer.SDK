d4b1dfdba18e2063f26fb28b92d01fa5375e5eb3
Hykilpikonna <admin@moecraft.cc>
2019-08-29 15:21:30 +0800

    [+] Encapsulate method to post a form

--- src/main/java/org/hydev/veracross/sdk/GeneralHttpClient.java
+++ src/main/java/org/hydev/veracross/sdk/GeneralHttpClient.java
@@ -1,11 +1,18 @@
 package org.hydev.veracross.sdk;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
 
 /**
  * TODO: Write a description for this class!
@@ -49,5 +56,30 @@ public class GeneralHttpClient
 
         // Build http client
         httpClient = builder.build();
+    }
+
+    /**
+     * Post a form
+     *
+     * @param client HTTP Client
+     * @param url Request URL.
+     * @param entity Form entity.
+     * @return HTTP Response
+     */
+    protected HttpResponse postForm(HttpClient client, String url, UrlEncodedFormEntity entity) throws IOException
+    {
+        // Create a post request.
+        HttpPost request = new HttpPost(url);
+
+        // Add headers
+        request.addHeader("content-type", "application/x-www-form-urlencoded");
+
+        // Create from entity from param pairs
+        request.setEntity(entity);
+
+        // Send request
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        return response;
     }
 }
