db6374d912693109376bd3aadd9fb16801870edc
Hykilpikonna <admin@moecraft.cc>
2019-08-29 18:40:48 +0800

    [O] Throw the exception instead of ignoring it

--- src/main/java/org/hydev/veracross/sdk/VeracrossHttpClient.java
+++ src/main/java/org/hydev/veracross/sdk/VeracrossHttpClient.java
@@ -24,19 +24,14 @@ public class VeracrossHttpClient extends GeneralHttpClient
      * @param username Username
      * @param ssoToken SJP Single sign on token
      */
-    public void loginSJP(String username, String ssoToken)
+    public void loginSJP(String username, String ssoToken) throws IOException
     {
-        try
-        {
-            // Post response
-            HttpResponse response = postForm("https://portals.veracross.com/sjp/login?WCI=Login&WCE=Submit", null,
-                            "username", username,
-                            "token", ssoToken);
+        // Post response
+        HttpResponse response = postForm("https://portals.veracross.com/sjp/login?WCI=Login&WCE=Submit", null,
+                        "username", username,
+                        "token", ssoToken);
+    }
 
-            int status = response.getStatusLine().getStatusCode();
-            String responseText = getResponseBody(response);
-        }
-        catch (IOException e)
         {
             // There is not much to do.
             throw new RuntimeException("Veracross SDK: Login Failed", e);
