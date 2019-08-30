3b1d50996f5efdcae9ffea899aedb7c36b6c7178
Hykilpikonna <admin@moecraft.cc>
2019-08-30 09:08:47 +0800

    [+] Create method to parse a list of courses from the html

--- src/main/java/org/hydev/veracross/sdk/VeracrossHtmlParser.java
+++ src/main/java/org/hydev/veracross/sdk/VeracrossHtmlParser.java
@@ -1,5 +1,13 @@
 package org.hydev.veracross.sdk;
 
+import org.hydev.veracross.sdk.model.VeracrossCourse;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,7 +25,48 @@ import java.util.regex.Pattern;
 public class VeracrossHtmlParser
 {
     private static final Pattern URL_NUMBER_PATTERN = Pattern.compile("(?<=/).[0-9]*(?=/)");
-    
+
+    /**
+     * Parse courses information from the html on the main page.
+     *
+     * @param mainPageHtml Html string on the main page.
+     * @return A list of courses
+     */
+    public static List<VeracrossCourse> parseCourses(String mainPageHtml)
+    {
+        // Create result list.
+        List<VeracrossCourse> result = new ArrayList<>();
+
+        // Parse with Jsoup
+        Document doc = Jsoup.parse(mainPageHtml);
+
+        // Search for class list elements.
+        Elements courseElements = doc.select(".class-list.clear > li");
+
+        // Loops through each course to find detailed information.
+        for (Element courseElement : courseElements)
+        {
+            // Find course name element and extract the course name.
+            Element courseNameElement = courseElement.selectFirst(".class-name");
+            String courseName = courseNameElement.html();
+
+            // Find teacher name element and extract the teacher name.
+            String teacherName = courseElement.selectFirst(".teacher-name").html();
+
+            // Find course information link, and find the course id in it.
+            long courseId = findNumberInUrl(courseNameElement.attr("href"));
+
+            // Find assignments info link, and find the assignment id in it.
+            long assignmentsId = findNumberInUrl(courseElement.selectFirst(".view-assignments").attr("href"));
+
+            // Add to result.
+            result.add(VeracrossCourse.builder().name(courseName).teacherName(teacherName)
+                    .id(courseId).assignmentsId(assignmentsId).build());
+        }
+
+        return result;
+    }
+
     /**
      * Find the first number in a url.
      *
