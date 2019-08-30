b6297be0bfc035e4fed99d2403e10f14d2a58e1b
Hykilpikonna <admin@moecraft.cc>
2019-08-30 16:22:28 +0800

    [O] Refactor buildURL method with the apache HC URIBuilder class

--- src/main/java/org/hydev/veracross/sdk/utils/UrlUtils.java
+++ src/main/java/org/hydev/veracross/sdk/utils/UrlUtils.java
@@ -1,7 +1,9 @@
 package org.hydev.veracross.sdk.utils;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import org.apache.http.client.utils.URIBuilder;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 
 /**
  * TODO: Write a description for this class!
@@ -18,36 +20,32 @@ public class UrlUtils
     /**
      * Build url params with a map of params.
      *
+     * @param base Base url
      * @param params Param key value pairs (Eg. ["key", "value"])
      * @return URL params
      */
-    public static String buildParams(Object... params)
+    public static String buildURL(String base, Object... params) throws MalformedURLException
     {
         // Null / empty cases
         if (params == null || params.length < 2) return "";
 
         try
         {
-            // Create result variable
-            StringBuilder result = new StringBuilder("?");
+            // Create url builder
+            URIBuilder builder = new URIBuilder(base);
 
+            // Add params
             for (int i = 0; i < params.length; i += 2)
             {
-                // Append "&" if it is not the first
-                if (i != 0) result.append("&");
-
-                // Append the param pair
-                result.append(params[i]).append("=").append(params[i + 1]);
+                builder.addParameter(params[i].toString(), params[i + 1].toString());
             }
 
-            // Return result
-            return URLEncoder.encode(result.toString(), "UTF-8");
+            // Return the result.
+            return builder.build().toURL().toString();
         }
-        catch (UnsupportedEncodingException e)
+        catch (URISyntaxException e)
         {
-            // This should never happen.
-            // If it does, there's nothing to do with it.
-            throw new RuntimeException(e);
+            throw new MalformedURLException("URI Syntax error: " + e.getMessage());
         }
     }
 }
