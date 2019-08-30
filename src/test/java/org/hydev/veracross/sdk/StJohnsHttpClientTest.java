74af4f8231b5c44992a87c41b74e07fcb3278a14
Hykilpikonna <admin@moecraft.cc>
2019-08-30 21:07:36 +0800

    [T] Expand tester to cover more stuff

--- src/test/java/org/hydev/veracross/sdk/StJohnsHttpClientTest.java
+++ src/test/java/org/hydev/veracross/sdk/StJohnsHttpClientTest.java
@@ -1,10 +1,14 @@
 package org.hydev.veracross.sdk;
 
 import com.google.gson.Gson;
+import org.hydev.veracross.sdk.model.VeracrossAssignments;
+import org.hydev.veracross.sdk.model.VeracrossCalendarEvent;
 import org.hydev.veracross.sdk.model.VeracrossCourse;
+import org.hydev.veracross.sdk.model.VeracrossMessage;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -48,6 +52,7 @@ class StJohnsHttpClientTest
     @Order(1)
     void login() throws IOException
     {
+        // Login St Johns
         stJohns.login(username, password);
     }
 
@@ -55,6 +60,7 @@ class StJohnsHttpClientTest
     @Order(2)
     void veracross() throws IOException
     {
+        // Login Veracross
         veracross = stJohns.veracrossLoginSSO();
     }
 
@@ -62,7 +68,50 @@ class StJohnsHttpClientTest
     @Order(3)
     void veracrossCourses() throws IOException
     {
+        // Get courses
         List<VeracrossCourse> courses = veracross.getCourses();
-        System.out.println(new Gson().toJson(courses));
+        log(courses);
+    }
+
+    @Test
+    @Order(4)
+    void veracrossAssignments() throws IOException
+    {
+        // Get assignments of the course at index 1 of the list.
+        VeracrossAssignments assignments = veracross.getAssignments(10934147);
+        log(assignments);
+    }
+
+    @Test
+    @Order(5)
+    void veracrossMessages() throws IOException
+    {
+        // Get messages starting at index 0.
+        List<VeracrossMessage> messages = veracross.getMessages(0);
+        log(messages);
+    }
+
+    @Test
+    @Order(6)
+    void veracrossEvents1() throws IOException
+    {
+        // Get calendar events from 5 days ago to 5 days later.
+        List<VeracrossCalendarEvent> events = veracross.getEvents(-5, 5);
+        log(events);
+    }
+
+    @Test
+    @Order(7)
+    void veracrossEvents2() throws IOException
+    {
+        // Get calendar events from 5 days ago to 5 days later.
+        List<VeracrossCalendarEvent> events = veracross.getEvents(new Date(), new Date());
+        log(events);
+    }
+
+    private static void log(Object object)
+    {
+        System.out.println(String.valueOf(new Gson().toJson(object)));
+        System.out.println("--------======= LINE OF SEPARATION =======--------");
     }
 }
