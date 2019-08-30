9fb515056d77147bf1ed195d237ee2c6ea38bb14
Hykilpikonna <admin@moecraft.cc>
2019-08-30 10:04:23 +0800

    [+] Create method to build parameters

--- src/main/java/org/hydev/veracross/sdk/utils/UrlUtils.java
+++ src/main/java/org/hydev/veracross/sdk/utils/UrlUtils.java
@@ -15,4 +15,39 @@ import java.net.URLEncoder;
  */
 public class UrlUtils
 {
+    /**
+     * Build url params with a map of params.
+     *
+     * @param params Param key value pairs (Eg. ["key", "value"])
+     * @return URL params
+     */
+    public static String buildParams(Object... params)
+    {
+        // Null / empty cases
+        if (params == null || params.length < 2) return "";
+
+        try
+        {
+            // Create result variable
+            StringBuilder result = new StringBuilder("?");
+
+            for (int i = 0; i < params.length; i += 2)
+            {
+                // Append "&" if it is not the first
+                if (i != 0) result.append("&");
+
+                // Append the param pair
+                result.append(params[i]).append("=").append(params[i + 1]);
+            }
+
+            // Return result
+            return URLEncoder.encode(result.toString(), "UTF-8");
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            // This should never happen.
+            // If it does, there's nothing to do with it.
+            throw new RuntimeException(e);
+        }
+    }
 }
