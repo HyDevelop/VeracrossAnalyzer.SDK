2f4c33fd3dba8ef08c66bcd8de94b409a2631be0
Hykilpikonna <admin@moecraft.cc>
2019-08-30 10:04:08 +0800

    [+] Create url utils

---
+++ src/main/java/org/hydev/veracross/sdk/utils/UrlUtils.java
@@ -0,0 +1,18 @@
+package org.hydev.veracross.sdk.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * TODO: Write a description for this class!
+ * <p>
+ * Class created by the HyDEV Team on 2019-08-30!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-08-30 09:32
+ */
+public class UrlUtils
+{
+}
