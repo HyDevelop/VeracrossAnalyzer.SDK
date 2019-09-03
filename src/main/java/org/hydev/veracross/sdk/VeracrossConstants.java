e4d26679a6624cf2a79e14d14d86a4a87532e2f7
Hykilpikonna <admin@moecraft.cc>
2019-09-03 16:16:32 -0400

    [+] Add version constant

--- src/main/java/org/hydev/veracross/sdk/VeracrossConstants.java
+++ src/main/java/org/hydev/veracross/sdk/VeracrossConstants.java
@@ -16,4 +16,10 @@ public class VeracrossConstants
      * The user agent that is used with the http client.
      */
     public static String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36";
+
+    /**
+     * The version of THIS MODULE. Other modules of the Veracross
+     * analyzer project have different versions.
+     */
+    public static String VERSION = "1.2.0.97";
 }
