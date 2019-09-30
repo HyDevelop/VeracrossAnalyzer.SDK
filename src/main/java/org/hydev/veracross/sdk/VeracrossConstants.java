196b6d6a9a63b48d92a843c38c960430ee3a35fc
Hykilpikonna <admin@moecraft.cc>
2019-09-30 18:20:06 -0400

    [U] Release v2.1.0.181

--- src/main/java/org/hydev/veracross/sdk/VeracrossConstants.java
+++ src/main/java/org/hydev/veracross/sdk/VeracrossConstants.java
@@ -21,5 +21,5 @@ public class VeracrossConstants
      * The version of THIS MODULE. Other modules of the Veracross
      * analyzer project have different versions.
      */
-    public static String VERSION = "2.0.0.169";
+    public static String VERSION = "2.1.0.181";
 }
