f60a694c93ad791381eb2d567afd5e4693e2b78a
Hykilpikonna <admin@moecraft.cc>
2019-10-05 14:54:52 -0400

    [-] Remove suffix list and level map

--- src/main/java/org/hydev/veracross/sdk/model/StJohnsCourse.java
+++ src/main/java/org/hydev/veracross/sdk/model/StJohnsCourse.java
@@ -3,9 +3,6 @@ package org.hydev.veracross.sdk.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * This data model class represents a course in St. John's.
  * <p>
@@ -20,25 +17,6 @@ import java.util.Map;
 @Data
 public class StJohnsCourse extends VeracrossCourse
 {
-    private static final String[][] LEVEL_ENDING_ALIAS =
-    {
-            {"H", " h", " honors"},
-            {"A", " a", " acc", " accelerated"},
-            {"CP", " c", " college prep"}
-    };
-
-    private static final Map<String, Float> LEVEL_SCALE_MAP;
-
-    static
-    {
-        // Initialize level scale map
-        LEVEL_SCALE_MAP = new HashMap<>();
-        LEVEL_SCALE_MAP.put("AP", 1f);
-        LEVEL_SCALE_MAP.put("H", 0.75f);
-        LEVEL_SCALE_MAP.put("A", 0.5f);
-        LEVEL_SCALE_MAP.put("CP", 0.25f);
-    }
-
     // Level of the course
     private String level;
 
