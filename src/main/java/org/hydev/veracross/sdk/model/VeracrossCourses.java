31b8dc6cff432dc93589b6c9c919cc6dbf66f141
Hykilpikonna <admin@moecraft.cc>
2019-11-29 14:28:43 -0500

    [+] Create VeracrossCourses class

---
+++ src/main/java/org/hydev/veracross/sdk/model/VeracrossCourses.java
@@ -0,0 +1,24 @@
+package org.hydev.veracross.sdk.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+
+/**
+ * TODO: Write a description for this class!
+ * <p>
+ * Class created by the HyDEV Team on 2019-11-29!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-11-29 07:46
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class VeracrossCourses extends ArrayList<VeracrossCourse>
+{
+    private String usernameEmail;
+    private long personPk;
+}
