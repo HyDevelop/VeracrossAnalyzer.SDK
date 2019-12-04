71ca5701c89d8ee02da0ad3947ed718ce5bff4fd
Hykilpikonna <admin@moecraft.cc>
2019-12-03 19:14:00 -0500

    [+] Create VeracrossPerson class

---
+++ src/main/java/org/hydev/veracross/sdk/model/VeracrossPerson.java
@@ -0,0 +1,22 @@
+package org.hydev.veracross.sdk.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * TODO: Write a description for this class!
+ * <p>
+ * Class created by the HyDEV Team on 2019-12-03!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-12-03 19:05
+ */
+@Data
+@AllArgsConstructor
+public class VeracrossPerson
+{
+    private String username;
+    private Integer personPk;
+}
